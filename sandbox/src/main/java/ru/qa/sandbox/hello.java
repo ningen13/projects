16e88e533f8b6c7e17bf1bb8d8698c3a5aaa4c6c
ningen13 <ningen13@mail.ru>
2020-06-21 23:28:56 +0300

    added a few functions

--- sandbox/src/main/java/ru/qa/sandbox/hello.java
+++ sandbox/src/main/java/ru/qa/sandbox/hello.java
@@ -4,6 +4,28 @@ public class hello {
 
 public static void main(String[] args) {
 
-System.out.println("helloworldffs");
+    hello("wut");
+    hello("there");
+    hello("Aleksey");
+
+double length = 3;
+System.out.println("Площадь квардрата со стороной " + length + " равна " + areasq(length));
+
+    double a = 5;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + arearec(a, b));
+
+}
+
+public static void hello (String someone) {
+    System.out.println("Hello, " + someone + "!");
+}
+
+public static double areasq(double length) {
+    return length * length;
+}
+
+public static double arearec(double a, double b) {
+    return a * b;
 }
 }
