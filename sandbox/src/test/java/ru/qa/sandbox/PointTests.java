fc29c850f96f77b357a83cb4385bfc1f926b8f40
ningen13 <ningen13@mail.ru>
2020-07-03 20:44:13 +0300

    tests for points added

---
+++ sandbox/src/test/java/ru/qa/sandbox/PointTests.java
@@ -0,0 +1,25 @@
+package ru.qa.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+
+    public void testDistance() {
+
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 0);
+
+        Assert.assertEquals(p1.distance(p2), 0);
+    }
+
+    public void testDistance2() {
+
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(4, 4);
+
+        Assert.assertEquals(p1.distance(p2), 4);
+    }
+}
