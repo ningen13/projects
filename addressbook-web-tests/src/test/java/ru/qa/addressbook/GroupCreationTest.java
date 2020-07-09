7eff67db106b5bcfdcfc4c4c67b9ce2305fb60f8
ningen13 <ningen13@mail.ru>
2020-07-10 01:29:44 +0300

    refactoring: additional class created

--- addressbook-web-tests/src/test/java/ru/qa/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/GroupCreationTest.java
@@ -13,43 +13,64 @@ public class GroupCreationTest {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
-    //wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    //wd.findElement(By.id("nav")).click();
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
-    //wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testGroupCreation() throws Exception {
 
-    wd.findElement(By.xpath("//body")).click();
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
+    openGroupPage();
+    createGroup();
+    fillGroupForm(new GroupData("group1", "groupleader1", "groupfooter"));
+    submitGroupCreation();
+    returnToGroupPage();
+
+  }
+
+  private void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("group1");
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("groupleader1");
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("groupfooter");
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     wd.findElement(By.id("content")).click();
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("group page")).click();
-    wd.findElement(By.xpath("//div[@id='content']/form/span")).click();
-    wd.findElement(By.name("selected[]")).click();
-    wd.findElement(By.name("selected[]")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=selected[] | ]]
-    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void createGroup() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void openGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
+    logout();
     wd.quit();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
   }
 
   private boolean isElementPresent(By by) {
