82e92dc1d66d8e400314b91e173cdff430c48e41
ningen13 <ningen13@mail.ru>
2020-07-16 21:33:49 +0300

    cycles added

---
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/PrimeTests.java
@@ -0,0 +1,25 @@
+package ru.qa.addressbook.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import qa.sandbox.Primes;
+
+public class PrimeTests {
+
+    @Test
+    public void testPrimes() {
+        Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+    }
+
+
+    @Test
+    public void testNonPrimes() {
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE -2));
+    }
+
+    @Test(enabled = false)
+    public void testPrimeLong() {
+        long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    }
+}
