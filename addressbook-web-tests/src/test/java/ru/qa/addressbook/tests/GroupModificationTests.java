dc4b4f685c16dc0d6daaa148642fa40e46fab2a3
ningen13 <ningen13@mail.ru>
2020-07-19 18:03:20 +0300

    Revert "get maximum element"
    This reverts commit 441cd087ab92ee549eff74445089d669e4635c9d.

--- addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupModificationTests.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.qa.addressbook.model.GroupData;
 
-import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -19,15 +18,10 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        GroupData group = new GroupData("group10", "groupleader10", "groupfooter0", before.get(before.size() - 1).getId());
-        app.getGroupHelper().fillGroupForm(group);
+        app.getGroupHelper().fillGroupForm(new GroupData("group10", "groupleader10", "groupfooter0"));
         app.getGroupHelper().submitGroupModification();
         app.getNavigationHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
-
-        before.remove(before.size() - 1);
-        before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
