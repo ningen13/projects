bea39a9f53d66a95b600563984565eff65a2f616
ningen13 <ningen13@mail.ru>
2020-07-27 22:24:52 +0300

    5.5

--- addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupCreationTest.java
@@ -5,8 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.qa.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupCreationTest extends TestBase {
 
@@ -14,19 +13,16 @@ public class GroupCreationTest extends TestBase {
   public void testGroupCreation() throws Exception {
 
     app.goTo().GroupPage();
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     GroupData group = new GroupData().withName("group1");
     app.group().createGroup();
-    List<GroupData> after = app.group().list();
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size() + 1);
+    group.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
 
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
+
     before.add(group);
-
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
-
     Assert.assertEquals(before, after);
   }
 }
