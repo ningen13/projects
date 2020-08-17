d9e740cdbc08738856becc51b1d2be8cb7c2e14e
ningen13 <ningen13@mail.ru>
2020-08-17 15:58:58 +0300

    6.11 logger added

--- addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupCreationTest.java
@@ -2,6 +2,8 @@ package ru.qa.addressbook.tests;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.qa.addressbook.model.GroupData;
@@ -16,6 +18,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
+
+
 
 
   /*@DataProvider
--- addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/TestBase.java
@@ -1,11 +1,17 @@
 package ru.qa.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.qa.addressbook.appmanager.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
 
@@ -19,14 +25,26 @@ public class TestBase {
         }
     }
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite (alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
     }
 
 }
